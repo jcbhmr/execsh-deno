11e049852c64a031012fd5c9112d39d5cd3e7f00
Jacob Hummer <jcbhmr@outlook.com>
2022-07-14 22:10:37 +0000

    Remove assert dependency
    Prefer to fail when function is executed, not before?

--- .ts
+++ .ts
@@ -1,9 +1,5 @@
-import assert from "assert"
 import shellEscape from "shell-escape"
 import ShellResponse from "./shell-response.ts"
-
-// Can only be run in Deno
-assert("Deno" in globalThis)
 
 export default function execsh(strings: TemplateStringsArray, ...inserts: Array<unknown>) {
     const command = inserts.flatMap((insert, index) => [strings[index], shellEscape(String(insert))]).concat([strings.at(-1)]).join(" ")
